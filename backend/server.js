a7c33e0e1a53e9fa4ab6d39315901f2076d4181c
Yair Yakobson <yairnet92@gmail.com>
2024-08-01 12:13:33 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,11 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get("*", (req, res) =>{
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) =>{
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
